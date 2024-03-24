5b5a3ce0aaff6356bd7837a337fe2664fc3f7c49
Rio Prayoga <yoprayogap@gmail.com>
2024-03-24 12:58:42 +0700

    day 4

---
+++ 4-sentece-smash.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/solutions/javascript
+
+const smash = (words) => words.join(" ");
+
+console.log(smash(["hello", "world", "this", "is", "great"]));
